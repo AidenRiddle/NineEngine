a4cee68366b4842c703b08bc8c1d46ba9b4b5f50
Joe <jahmad92@hotmail.com>
2024-02-25 00:10:37 +0100

    ExpandableCard in Hierarchy and Inspector

--- Core/UICore/Hierarchy/Components/item.js
+++ Core/UICore/Hierarchy/Components/item.js
@@ -1,70 +1,37 @@
-import { $ArrowExpand } from "../../Common/commonGui.js";
-import { GuiHandle, GuiContext } from "../../gui.js";
+import { $ExpandableCard } from "../../Common/commonGui.js";
+import { GuiContext, GuiHandle } from "../../gui.js";
 
-let tabIndexCounter = -1;
 const showSubfolderByDefault = true;
 
 export class $Folder extends GuiHandle {
     /**
      * @param {GuiContext} gui 
      * @param {HTMLElement} root
+     * @param {GuiHandle} handle
      */
-    static builder(gui, root) {
-        tabIndexCounter++;
+    static builder(gui, root, handle) {
         const value = gui.state("value");
         const subfolders = gui.state("subfolders");
-        const callback = gui.state("callback");
 
-        root.style.display = "flex";
-        root.style.flexDirection = "column";
-        root.style.alignItems = "flex-start";
-        root.onclick = callback;
-        root.onfocus = function (e) { e.target.style.background = "blue"; };
-        root.onblur = function (e) { e.target.style.background = "none"; };
-
-        const subfolderContainer = gui.node("div", div => {
-            div.style.display = (showSubfolderByDefault) ? "block" : "none";
-            div.style.padding = "0px 0px 0px 10px";
-            if (subfolders != null && subfolders.length > 0) {
-                gui.bake(div, subfolders);
-            }
+        const titleCard = gui.node("div", div => {
+            div.append(
+                gui.node("p", p => { p.textContent = value; })
+            )
         });
 
-        const displayCard = gui.node("div", div => {
-            div.tabIndex = tabIndexCounter;
-            div.style.display = "flex";
-            div.style.flexDirection = "row";
-            div.style.alignItems = "center";
-            div.style.padding = "0px";
-            div.style.width = "100%";
-            div.onclick = callback;
-            div.onfocus = function (e) { div.style.background = "blue"; };
-            div.onblur = function (e) { div.style.background = "none"; };
+        if (subfolders != null && subfolders.length > 0) {
+            const subfolderContainer = gui.node("div", div => {
+                div.style.display = (showSubfolderByDefault) ? "block" : "none";
+                div.style.padding = "0px 0px 0px 10px";
+                gui.bake(div, subfolders);
+            });
+            handle.set("subCard", subfolderContainer);
+        }
 
-            div.append(
-                gui.node("div", div => {
-                    $ArrowExpand.builder(gui, div);
-                    div.onclick = () => {
-                        if (subfolderContainer.style.display === "none") {
-                            subfolderContainer.style.display = "block";
-                        } else {
-                            subfolderContainer.style.display = "none";
-                        }
-                    }
+        handle.set("titleCard", titleCard);
+        handle.set("onselect", gui.state("callback"));
+        handle.set("highlightOnSelect", true);
 
-                    if (subfolders == null || subfolders.length == 0) {
-                        div.style.visibility = "hidden";
-                    }
-                }),
-                gui.node("div", div => {
-                    div.append(
-                        gui.node("p", p => { p.textContent = value; })
-                    )
-                }),
-            );
-        })
-
-        root.append(displayCard);
-        root.append(subfolderContainer);
+        $ExpandableCard.builder(gui, root, handle);
     }
 }
