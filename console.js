44c41f6566f31794a9d52ef18bd8e89f7ce6bcf4
Joe <jahmad92@hotmail.com>
2024-02-24 08:08:16 +0100

    More console commands

--- console.js
+++ console.js
@@ -1,3 +1,12 @@
+import { MaterialStorage } from "./Core/DataStores/materialStore.js";
+import { MeshStorage } from "./Core/DataStores/meshStore.js";
+import { ModelStorage } from "./Core/DataStores/modelStore.js";
+import { ProgramStorage } from "./Core/DataStores/programStore.js";
+import { SceneStorage } from "./Core/DataStores/sceneStore.js";
+import { ScriptStorage } from "./Core/DataStores/scriptStore.js";
+import { ShaderStorage } from "./Core/DataStores/shaderStore.js";
+import { TextureStorage } from "./Core/DataStores/textureStore.js";
+import Gpu from "./Core/GECore/Gpu/gpu.js";
 import { ScriptManager } from "./Core/GameCore/WebAssembly/scriptManager.js";
 import { RunningInstance } from "./Core/GameCore/runningInstance.js";
 import Resources from "./FileSystem/resources.js";
@@ -28,6 +37,23 @@ class NineEngineConsole {
 
     shadowHalfSamples(n) {
         AppSettings.shadow_halfSamples = n;
+    }
+
+    logDataStores() {
+        const gpuLog = {
+            "TotalBufferAllocationSize": Gpu.bufferAllocation
+        }
+        console.log("Gpu:", gpuLog,
+            "\n",
+            "\nMaterials:", MaterialStorage.debug(),
+            "\nMeshes:", MeshStorage.debug(),
+            "\nModels:", ModelStorage.debug(),
+            "\nPrograms:", ProgramStorage.debug(),
+            "\nScenes:", SceneStorage.debug(),
+            "\nScripts:", ScriptStorage.debug(),
+            "\nShaders:", ShaderStorage.debug(),
+            "\nTextures:", TextureStorage.debug()
+        );
     }
 }
 globalThis.nine = new NineEngineConsole();
