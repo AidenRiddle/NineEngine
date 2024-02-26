664ee4f0dbb2f4c538f44b9541a93073c55b4062
AidenR <jahmad92@hotmail.com>
2024-02-26 08:29:22 +0100

    Revert "ModelStorage Get bug fix"
    This reverts commit bb5a20a9b9dd91035a4a14efe70684c261856951.

--- Core/DataStores/modelStore.js
+++ Core/DataStores/modelStore.js
@@ -41,7 +41,7 @@ export class ModelStorage extends DataStorage {
     static pack() {
         const payload = {}
         for (const key of super.keys()) {
-            const model = this.storage.get(key)
+            const model = this.storage[key]
             payload[key] = {
                 meshId: model.meshId,
                 materials: model.materials
