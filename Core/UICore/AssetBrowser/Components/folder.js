84b3f614eb0dbb257d46d78ca674b97e7ff64451
AidenR <jahmad92@hotmail.com>
2024-02-26 08:29:22 +0100

    Revert "Asset browser tree uses CommonGui"
    This reverts commit 0890b3ccc06932c1e85c28ad0a0346f0bf1124e9.

--- Core/UICore/AssetBrowser/Components/folder.js
+++ Core/UICore/AssetBrowser/Components/folder.js
@@ -1,58 +1,107 @@
-import { $ExpandableCard } from "../../Common/commonGui.js";
 import { GuiContext, GuiHandle } from "../../gui.js";
 
 const showSubfolderByDefault = true;
+const downArrow = "/Core/UICore/AssetBrowser/Icons/211687_down_arrow_icon.png";
+const rightArrow = "/Core/UICore/AssetBrowser/Icons/211607_right_arrow_icon.png";
 
-const imgThumbnail = "/Resources/Textures/unavailable-image.jpg";
+const imgThumbnail = "/Resources/Textures/skull/jl9.jpg";
 
 var tabIndexCounter = -1;
+
+function toggleFolderListView(arrowImg, node) {
+    if (node.style.display === "none") {
+        node.style.display = "block";
+        arrowImg.style.transform = "rotate(0)";
+    } else {
+        node.style.display = "none";
+        arrowImg.style.transform = "rotate(-90deg)";
+    }
+}
 
 export class $Folder extends GuiHandle {
     /**
      * @param {GuiContext} gui 
      * @param {HTMLElement} root
-     * @param {GuiHandle} handle
      */
-    static builder(gui, root, handle) {
+    static builder(gui, root) {
         const value = gui.state("value");
         const subfolders = gui.state("subfolders");
+        const callback = gui.state("callback");
         const adderCallback = gui.state("adderCallback");
         const moveFileCallBack = gui.state("moveFileCallBack");
 
+        tabIndexCounter++;
+        const subfolderContainer = gui.node("div", div => {
+            div.id = "folder-subfolder-container";
+            div.style.display = (showSubfolderByDefault) ? "block" : "none";
+            div.style.padding = "0px 0px 0px 10px";
+            gui.bake(div, subfolders);
+        });
+
+        const arrowImg = gui.node("img", img => {
+            img.src = downArrow;
+            img.style.width = "15px";
+            img.style.height = "15px";
+            img.style.transition = "all 0.15s ease-out";
+            img.style.transform = (showSubfolderByDefault) ? "rotate(0)" : "rotate(-90deg)";
+            img.style.opacity = (subfolders.length > 0) ? "1" : "0";
+            img.style.filter = "invert(100%)";
+            img.onclick = () => {
+                toggleFolderListView(img, subfolderContainer);
+            }
+        });
+
         const addFolderButton = gui.node("div", div => {
-            div.id = "folder-add";
-            div.append(gui.node("p", p => { p.textContent = "+"; }));
-            // div.style.display = "none";
-            div.onclick = adderCallback;
+            div.id = "folder-add",
+                div.append(gui.node("p", p => { p.textContent = "+"; })),
+                div.style.display = "none"
+            div.onclick = adderCallback
         })
 
+        // This was a draggableReciever with moveFileCallBack
         const wrapper = gui.node("div", div => {
             div.id = "folder-header";
+            div.tabIndex = tabIndexCounter.toString();
             div.style.display = "flex";
             div.style.flexDirection = "row";
             div.style.alignItems = "center";
-            div.style.gap = "5px";
+            div.style.padding = "0px";
+            div.style.width = "200px";
+            div.onclick = callback;
+            div.onfocus = function (e) {
+                e.target.style.background = "blue";
+                addFolderButton.style.display = "block";
+            }
+            div.onblur = function (e) {
+                e.target.style.background = "none";
+                addFolderButton.style.display = "none";
+            }
             div.append(
-                gui.node("p", p => { p.textContent = value; }),
+                gui.node("div", div => {
+                    div.id = "folder-arrow";
+                    div.append(arrowImg);
+                }),
+                gui.node("div", div => {
+                    div.id = "folder-name";
+                    div.append(
+                        gui.node("p", p => {
+                            p.textContent = value;
+                        })
+                    )
+                }),
                 addFolderButton,
             )
-        });
+        })
 
-        if (subfolders != null && subfolders.length > 0) {
-            const subfolderContainer = gui.node("div", div => {
-                div.id = "folder-subfolder-container";
-                div.style.display = (showSubfolderByDefault) ? "block" : "none";
-                div.style.padding = "0px 0px 0px 10px";
-                gui.bake(div, subfolders);
-            });
-            handle.set("subCard", subfolderContainer);
-        }
-
-        handle.set("titleCard", wrapper);
-        handle.set("onselect", gui.state("callback"));
-        handle.set("highlightOnSelect", true);
-
-        $ExpandableCard.builder(gui, root, handle);
+        root.id = "folder";
+        root.style.display = "flex";
+        root.style.flexDirection = "column";
+        root.style.alignItems = "flex-start";
+        root.style.width = "100%";
+        root.append(
+            wrapper,
+            subfolderContainer,
+        )
     }
 }
 
@@ -94,8 +143,9 @@ export class $File extends GuiHandle {
             label.style.maxWidth = "100px";
             label.style.whiteSpace = "initial";
             // label.style.overflow = "initial";
+
+            this.onmouseup = (e) => { onclick(); }
         }
-        root.ondblclick = (e) => { onclick(); }
         root.onblur = function (e) {
             e.target.style.background = "none";
             label.style.maxWidth = childrenMaxWidth;
