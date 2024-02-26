c2ecc777a301e86ca3dd211b0fc4c4d8298c4743
AidenR <jahmad92@hotmail.com>
2024-02-26 08:29:22 +0100

    Revert "ExpandableCard in Hierarchy and Inspector"
    This reverts commit 5fea541a91208b9391eeeda76fdbdbc354254121.

--- Core/UICore/Hierarchy/Components/item.js
+++ Core/UICore/Hierarchy/Components/item.js
@@ -1,37 +1,70 @@
-import { $ExpandableCard } from "../../Common/commonGui.js";
-import { GuiContext, GuiHandle } from "../../gui.js";
+import { $ArrowExpand } from "../../Common/commonGui.js";
+import { GuiHandle, GuiContext } from "../../gui.js";
 
+let tabIndexCounter = -1;
 const showSubfolderByDefault = true;
 
 export class $Folder extends GuiHandle {
     /**
      * @param {GuiContext} gui 
      * @param {HTMLElement} root
-     * @param {GuiHandle} handle
      */
-    static builder(gui, root, handle) {
+    static builder(gui, root) {
+        tabIndexCounter++;
         const value = gui.state("value");
         const subfolders = gui.state("subfolders");
+        const callback = gui.state("callback");
 
-        const titleCard = gui.node("div", div => {
-            div.append(
-                gui.node("p", p => { p.textContent = value; })
-            )
+        root.style.display = "flex";
+        root.style.flexDirection = "column";
+        root.style.alignItems = "flex-start";
+        root.onclick = callback;
+        root.onfocus = function (e) { e.target.style.background = "blue"; };
+        root.onblur = function (e) { e.target.style.background = "none"; };
+
+        const subfolderContainer = gui.node("div", div => {
+            div.style.display = (showSubfolderByDefault) ? "block" : "none";
+            div.style.padding = "0px 0px 0px 10px";
+            if (subfolders != null && subfolders.length > 0) {
+                gui.bake(div, subfolders);
+            }
         });
 
-        if (subfolders != null && subfolders.length > 0) {
-            const subfolderContainer = gui.node("div", div => {
-                div.style.display = (showSubfolderByDefault) ? "block" : "none";
-                div.style.padding = "0px 0px 0px 10px";
-                gui.bake(div, subfolders);
-            });
-            handle.set("subCard", subfolderContainer);
-        }
+        const displayCard = gui.node("div", div => {
+            div.tabIndex = tabIndexCounter;
+            div.style.display = "flex";
+            div.style.flexDirection = "row";
+            div.style.alignItems = "center";
+            div.style.padding = "0px";
+            div.style.width = "100%";
+            div.onclick = callback;
+            div.onfocus = function (e) { div.style.background = "blue"; };
+            div.onblur = function (e) { div.style.background = "none"; };
 
-        handle.set("titleCard", titleCard);
-        handle.set("onselect", gui.state("callback"));
-        handle.set("highlightOnSelect", true);
+            div.append(
+                gui.node("div", div => {
+                    $ArrowExpand.builder(gui, div);
+                    div.onclick = () => {
+                        if (subfolderContainer.style.display === "none") {
+                            subfolderContainer.style.display = "block";
+                        } else {
+                            subfolderContainer.style.display = "none";
+                        }
+                    }
 
-        $ExpandableCard.builder(gui, root, handle);
+                    if (subfolders == null || subfolders.length == 0) {
+                        div.style.visibility = "hidden";
+                    }
+                }),
+                gui.node("div", div => {
+                    div.append(
+                        gui.node("p", p => { p.textContent = value; })
+                    )
+                }),
+            );
+        })
+
+        root.append(displayCard);
+        root.append(subfolderContainer);
     }
 }
--- Core/UICore/Inspector/Components/card.js
+++ Core/UICore/Inspector/Components/card.js
@@ -1,74 +1,85 @@
-import { $ExpandableCard } from "../../Common/commonGui.js";
+import { $ArrowExpand } from "../../Common/commonGui.js";
 import { GuiContext, GuiHandle } from "../../gui.js";
 
+const downArrow = "/Core/UICore/AssetBrowser/Icons/211687_down_arrow_icon.png";
 const checkboxFilled = "/Core/UICore/Inspector/Icons/8679969_checkbox_line_icon.png";
 const checkboxEmpty = "/Core/UICore/Inspector/Icons/8679886_checkbox_blank_line_icon.png";
 const componentIcon = "/Core/UICore/Inspector/Icons/754626_deadly_decoration_halloween_lethal_scary_icon.png";
 
-export class $InspectorCardIcon extends GuiHandle {
-    /**
-     * @param {GuiContext} gui 
-     * @param {HTMLElement} root
-     * @param {GuiHandle} handle
-     */
-    static builder(gui, root, handle) {
-        gui.makeRoot(gui.node("img", img => {
-            img.src = gui.state("iconSource");
-            img.style.width = "25px";
-            img.style.height = "25px";
-            if (gui.state("invert")) img.style.filter = "invert(100%)";
-        }));
+function toggleFolderListView(arrowImg, node) {
+    if (node.style.display === "none") {
+        node.style.display = "flex";
+        arrowImg.style.transform = "rotate(0)";
+        return true;
     }
+    
+    node.style.display = "none";
+    arrowImg.style.transform = "rotate(-90deg)";
+    return false;
 }
 
 export class $Card extends GuiHandle {
     /**
      * @param {GuiContext} gui 
      * @param {HTMLElement} root
-     * @param {GuiHandle} handle
      */
-    static builder(gui, root, handle) {
+    static builder(gui, root) {
         const title = gui.state("title");
         const isEnableable = gui.state("isEnableable");
+        const content = gui.getNode(gui.state("content"));
 
-        const icon = gui.getNode(new $InspectorCardIcon({ "iconSource": componentIcon, "invert": true }));
-        const label = gui.node("p", p => { p.textContent = title; });
-        const titleCard = gui.node("div", div => {
+        const icon = gui.node("img", img => {
+            img.src = componentIcon;
+            img.style.width = "25px";
+            img.style.height = "25px";
+            img.style.filter = "invert(100%)";
+        });
+
+        const label = gui.node("p", p => {
+            p.textContent = title;
+        });
+        
+        const wrapper = gui.node("div", div => {
             div.id = "card-header";
             div.style.display = "flex";
             div.style.flexDirection = "row";
             div.style.alignItems = "center";
-            div.style.gap = "5px";
+
+            $ArrowExpand.builder(gui, div);
+
+            div.onclick = function(e) {
+                if (content.style.display === "none") {
+                    content.style.display = "block";
+                    root.style.marginBottom = "30px";
+                } else {
+                    content.style.display = "none";
+                    root.style.marginBottom = "5px";
+                }
+            };
 
             div.append(icon, label);
         });
 
-        if (isEnableable) {
+        if(isEnableable){
             const enableCheckmark = gui.node("img", img => {
                 img.src = checkboxFilled;
                 img.style.width = "15px";
                 img.style.height = "15px";
                 img.style.filter = "invert(100%)";
-                img.onclick = function (e) {
+                img.onclick = function(e) {
                     e.target.src = (e.target.src.endsWith(checkboxEmpty)) ? checkboxFilled : checkboxEmpty
                 }
             });
-            titleCard.append(enableCheckmark);
+            wrapper.append(enableCheckmark);
         }
 
-        const card = gui.getNode(new $ExpandableCard({
-            "titleCard": titleCard,
-            "subCard": gui.state("content"),
-            "onselect": gui.state("callback"),
-            "onexpand": () => { root.style.marginBottom = "30px"; },
-            "oncollapse": () => { root.style.marginBottom = "5px"; },
-            "highlightOnSelect": true,
-        }));
+        root.id = "card";
+        root.style.display = "flex";
+        root.style.flexDirection = "column";
+        root.style.alignItems = "flex-start";
+        root.style.alignSelf = "stretch";
+        root.style.marginBottom = "30px";
 
-        card.id = "card";
-        card.style.alignSelf = "stretch";
-        card.style.marginBottom = "30px";
-
-        gui.makeRoot(card);
+        root.append(wrapper, content);
     }
 }
