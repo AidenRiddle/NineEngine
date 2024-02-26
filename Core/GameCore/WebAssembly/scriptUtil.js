d7e47e1f3a1cbf61cc23220589d9e81365e31256
AidenR <jahmad92@hotmail.com>
2024-02-26 08:29:22 +0100

    Revert "Forgot import"
    This reverts commit 4637a3df0d9b0fcf103457db8700734315f2c12a.

--- Core/GameCore/WebAssembly/scriptUtil.js
+++ Core/GameCore/WebAssembly/scriptUtil.js
@@ -1,5 +1,3 @@
-import { System } from "../../../settings.js";
-
 export class ScriptUtil {
     static logDev(...msgs) { System.log(System.dev_console_message_prefix, ...msgs); }
     static logDebug(...msgs) { System.log(System.debug_message_prefix, ...msgs); }
